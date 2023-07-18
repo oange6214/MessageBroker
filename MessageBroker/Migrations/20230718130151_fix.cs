451a6f7b5c8633127f3acbd1929d53e139e010cf
Jed <oange6214@gmail.com>
2023-07-19 01:26:53 +0800

    Add project files.

---
+++ MessageBroker/Migrations/20230718130151_fix.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MessageBroker.Migrations
+{
+    /// <inheritdoc />
+    public partial class fix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Messages",
+                newName: "TopicMessage");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TopicMessage",
+                table: "Messages",
+                newName: "Name");
+        }
+    }
+}
